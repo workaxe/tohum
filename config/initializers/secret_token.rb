363ad2dc6ebafb81ade5582fb81fe8ec1df0b4e1
Emir Alp <okebye@gmail.com>
2013-06-11 04:33:11 +0300

    kurulum

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tohum::Application.config.secret_token = '3bc977e3f2e2bfb05fb385f2cfb2d6af13713b772111e1090e1dc9cd742ccf5fc089694b8aa73d047326df65fcad59dfe11b2f469d4c922ba8a2981cedfd9df0'
